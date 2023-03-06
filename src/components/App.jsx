ce62b2c615e07d7ffa7df19fc989af61a59287eb
YevgeniiVashchenko <yevgenii.vashchenko@gmail.com>
2023-03-06 14:38:33 +0200

    add localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,36 @@
 import { Component } from 'react';
-import { GlobalStyles } from '../GlobalStyles';
+import { GlobalStyles } from './GlobalStyles';
 import 'modern-normalize';
 import { nanoid } from 'nanoid';
 
-import { ContactForm } from '../ContactForm/ContactForm';
-import { ContactList } from '../ContactList/ContactList';
-import { Filter } from '../Filter/Filter';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
 import { MainForm, MainTitle, Title } from './App.styled';
+import initialContacts from '../contacts.json';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+    console.log (savedContacts);
+    if (savedContacts !== null) {
+      const parsedContacts = JSON.parse(savedContacts);
+      this.setState({contacts: parsedContacts});
+      return;
+    }
+    this.setState({contacts: initialContacts});
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem ('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   handleSubmit = (values, { resetForm }) => {
     let newContact = values;
